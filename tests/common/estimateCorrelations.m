7393a028ce7af7bf559a15c9f59562f0f7a987b2
samy <kandasamy@cmu.edu>
2014-04-23 21:09:17 -0400

    added functions for estimating correlations

---
+++ tests/common/estimateCorrelations.m
@@ -0,0 +1,46 @@
+function corrGraph = estimateCorrelations(nodePot, ...
+  edgePot, edgeStruct, numBurnIn, numSamples)
+% This function measures the correlations between two variables in the graph and
+% outputs a graph whose weights are the estimated correlations.
+% @ Wenlu: when you are writing your function/ wrapper you could assume that the
+% input is a sparse Matrix.
+
+% nodePot and edgePot are the Node and Edge Potentials. These 2 along with
+% edgeStruct are needed by the UGM library for sampling. 
+% numBurnIn and numSamples are the number of MCMC samples for estimating the
+% correlations.
+
+  if ~exist('numBurnin', 'var')
+    numBurnIn = 100;
+  end
+  if ~exist('numSamples', 'var')
+    numSamples = 100000;
+  end
+
+  edgeStruct.maxIter = numSamples;
+  edgeStruct.useMex = 0;
+  samples = UGM_Sample_Gibbs(nodePot, edgePot, edgeStruct, numBurnIn);
+  samples = double(samples);
+  
+  estimMeans = mean(samples')';
+  estimStds = std(samples')';
+  estimStds = estimStds + double((estimStds == 0));
+
+  % Now for each edge in the graph, compute the correlations
+  corrGraph = sparse(edgeStruct.nEdges, edgeStruct.nEdges);
+  for edgeIter = 1:edgeStruct.nEdges
+    n1 = edgeStruct.edgeEnds(edgeIter, 1);
+    n2 = edgeStruct.edgeEnds(edgeIter, 2);
+
+    edgeSamples = samples([n1, n2], :);
+    edgeMeans = estimMeans([n1, n2]);
+    edgeStds = estimStds([n1, n2]);
+    covarVals = bsxfun(@minus, edgeSamples, edgeMeans);
+    covar = mean(covarVals(1,:) .* covarVals(2,:));
+    currCorr = covar/ prod(edgeStds);
+    corrGraph(n1, n2) = currCorr;
+    corrGraph(n2, n1) = currCorr;
+  end
+
+end
+
---
+++ tests/common/ut_estimateCorrelations.m
@@ -0,0 +1,29 @@
+% A unit test for EstimateCorrelations.m
+
+% Create the graph 
+nStates = 2;
+adjMat = sparse(5,5);
+adjMat(1,2) = 1;
+adjMat(1,3) = 1;
+adjMat(1,4) = 1;
+adjMat(3,4) = 1;
+adjMat(3,5) = 1;
+adjMat = adjMat + adjMat';
+% Create the Edge struct
+edgeStruct = UGM_makeEdgeStruct(adjMat, nStates);
+% Create Node potentials
+nodePot = 0.01 * ones(5, nStates);
+
+% Create Edge Potentials
+s = 100;
+edgePot = zeros(nStates, nStates, edgeStruct.nEdges);
+edgePot(:, :, 1) = s*[2 0.1; 0.1 2];
+edgePot(:, :, 2) = s*[0.5 0.5; 0.5 0.5];
+edgePot(:, :, 3) = s*[0.6 0.4; 0.3 0.7];
+edgePot(:, :, 4) = s*[0.5 1; 1 0.5];
+edgePot(:, :, 5) = s*[1 3; 3 1];
+
+% Now obtain the correlation graph
+corrGraph = estimateCorrelations(nodePot, edgePot, edgeStruct, 10, 10000);
+corrGraph,
+
